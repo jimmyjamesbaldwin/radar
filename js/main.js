7d7ee8350afe12c92f99a44ad049c671e9145c49
David Bain <davejbai@gmail.com>
2016-03-25 19:25:36 +0000

    Moved inline JS to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+function mapCalculation(){
+    var jsonInput = '[{"room":"PG07","availability":"1"},{"room":"PG06","availability":"0"}]';
+    var jsonParsed = $.parseJSON(jsonInput);
 
+    $.each(jsonParsed, function() {
+        console.log(this['room']);
+        var currentRoom = '#' + this['room'];
+        var currentRoomLabel = currentRoom + '-label';
+        if(this['availability'] === '0'){
+            $(currentRoom).css({ fill: "#A81C07"});
+            $(currentRoomLabel).css('color', 'white'); //TODO fix text colour
+        } else {
+            $(currentRoom).css({ fill: "#77DD77"});
+        }
+    });
+}
